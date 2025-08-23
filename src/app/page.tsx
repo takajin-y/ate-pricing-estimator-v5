3d6047d0c50128aeadf98f54a1809f8a3478f93d
takajin-y <ludwig0611@yahoo.co.jp>
2025-08-24 01:23:08 +0900

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { EstimatorV5 } from "@/components/EstimatorV5";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen p-6">
+      <EstimatorV5 />
+    </main>
+  );
+}
